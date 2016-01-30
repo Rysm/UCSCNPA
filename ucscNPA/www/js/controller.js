9760261a0f6005760873a09405ed5f699d124b06
Rysm <awan26@ucsc.edu>
2016-01-29 17:17:50 -0800

    JS up

---
+++ ucscNPA/www/js/controller.js
@@ -0,0 +1,9 @@
+//State controllers
+
+.controller('LoginCtrl', function($scope) {
+    $scope.data = {};
+ 
+    $scope.login = function() {
+        console.log("LOGIN user: " + $scope.data.username + " - PW: " + $scope.data.password);
+    }
+})
