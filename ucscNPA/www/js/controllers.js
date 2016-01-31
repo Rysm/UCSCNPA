005c6f5997068dd64a80ef3fe222d3fe19f3b71f
Rysm <awan26@ucsc.edu>
2016-01-30 22:16:54 -0800

    Finally it works swag
    swag

---
+++ ucscNPA/www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('🔍SearchCtrl', function($scope) {
+
+})
+      
+.controller('🎪RequestEventCtrl', function($scope) {
+
+})
+   
+.controller('createEventCtrl', function($scope) {
+
+})
+   
+.controller('editOrgCtrl', function($scope) {
+
+})
+   
+.controller('createOrgCtrl', function($scope) {
+
+})
+   
+.controller('profileCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('createAccountCtrl', function($scope) {
+
+})
+   
+.controller('forgotPasswordCtrl', function($scope) {
+
+})
+ 
---
+++ ucscNPA/www/js/routes.js
@@ -0,0 +1,151 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('menu.home', {
+      url: '/home',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/home.html',
+          controller: 'homeCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('menu.🔍Search', {
+      url: '/search',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/🔍Search.html',
+          controller: '🔍SearchCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+    .state('menu', {
+      url: '/home-menu',
+      abstract:true,
+      templateUrl: 'templates/menu.html'
+    })
+      
+    
+      
+        
+    .state('menu.🎪RequestEvent', {
+      url: '/request',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/🎪RequestEvent.html',
+          controller: '🎪RequestEventCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('menu.createEvent', {
+      url: '/create',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/createEvent.html',
+          controller: 'createEventCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('menu.editOrg', {
+      url: '/edit',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/editOrg.html',
+          controller: 'editOrgCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('menu.createOrg', {
+      url: '/page7',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/createOrg.html',
+          controller: 'createOrgCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('menu.profile', {
+      url: '/profile',
+      views: {
+        'side-menu21': {
+          templateUrl: 'templates/profile.html',
+          controller: 'profileCtrl'
+        }
+      }
+    })
+        
+      
+    
+      
+        
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'loginCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('createAccount', {
+      url: '/signup',
+      templateUrl: 'templates/createAccount.html',
+      controller: 'createAccountCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('forgotPassword', {
+      url: '/forgot',
+      templateUrl: 'templates/forgotPassword.html',
+      controller: 'forgotPasswordCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+});
