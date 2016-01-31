8bc30a50f4a27614e2b4cd03ffc77e566edba29a
Rysm <awan26@ucsc.edu>
2016-01-31 01:30:43 -0800

    Added org events

--- ucscNPA/www/js/app.js
+++ ucscNPA/www/js/app.js
@@ -5,7 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives','firebase'])
+
+//angular.module('ionic-firebase-seed', ['ionic', 'firebase'])
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,3 +23,50 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
     }
   });
 })
+
+// our firebase url
+.constant('FBURL', 'https://ucscnpa.firebaseio.com/')
+
+//create a firebase url
+//store events in it
+.factory('Messages', function($firebaseArray, FBURL, $window) {
+  var ref = new $window.Firebase(FBURL + '/messages');
+  return $firebaseArray(ref);
+})
+
+.controller('AppCtrl', function($scope, Auth, Messages) {
+
+
+  // ADD MESSAGES TO A SYNCHRONIZED ARRAY
+
+  // Bind messages to the scope
+  $scope.messages = Messages;
+
+  // Add a message to a synchronized array using $add with $firebaseArray
+  $scope.addEvent = function(name,desc,loc,phone,email) {
+      Messages.$add({
+        name: name,
+        email: email,
+        phone: phone,
+        location: loc
+      });
+      message.text = "";
+  };
+
+})
+
+.run(function($ionicPlatform, FBURL) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (FBURL === "https://DEFAULT.firebaseio.com/") {
+      angular.element(document.getElementById('app-content')).html('<h1>Please configure your Firebase URL in www/js/app.js before running!</h1>');
+    }
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
--- ucscNPA/www/js/controllers.js
+++ ucscNPA/www/js/controllers.js
@@ -1,46 +1,46 @@
 angular.module('app.controllers', [])
-  
+
 .controller('homeCtrl', function($scope) {
 
 })
-   
+
 .controller('🔍SearchCtrl', function($scope) {
 
 })
-      
+
 .controller('🎪RequestEventCtrl', function($scope) {
 
 })
-   
-.controller('createEventCtrl', function($scope) {
 
+.controller('createEventCtrl', function($scope, Auth, Messages) {
+
+  
 })
-   
+
 .controller('editOrgCtrl', function($scope) {
 
 })
-   
+
 .controller('createOrgCtrl', function($scope) {
 
 })
-   
+
 .controller('profileCtrl', function($scope) {
 
 })
-   
+
 .controller('loginCtrl', function($scope) {
 
 })
-   
+
 .controller('createAccountCtrl', function($scope) {
 
 })
-   
+
 .controller('forgotPasswordCtrl', function($scope) {
 
 })
-   
+
 .controller('mapsCtrl', function($scope) {
 
 })
- 
